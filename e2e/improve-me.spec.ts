db73592328b9736b873de265144ad1c32663906a
Yogesh Kumar Pilli <yogeshkumarpilli@gmail.com>
2023-10-15 20:48:41 +0200

    Introduced_PageObjectModel_For_CodeReusability

--- e2e/improve-me.spec.ts
+++ e2e/improve-me.spec.ts
@@ -1,49 +1,44 @@
-import { test, expect } from "@playwright/test";
 
 /**
  * This test runs but can be improved
  *
  * Find ways to improve this test
  */
+import { test, expect } from "@playwright/test";
+
+const BASE_URL = "http://localhost:5173/";
 
 test.describe("Patients", () => {
-  test("should have patient Laurence Fournier", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
-    const line = page.locator("tr", { hasText: "Laurence Fournier" });
-    const cells = line.locator("td");
-    await expect(cells.nth(0)).toHaveText("Laurence Fournier");
-    await expect(cells.nth(1)).toHaveText("Post TAVI");
-    await expect(cells.nth(2)).toHaveText("21");
-    await expect(cells.nth(3)).toHaveText("3/12/2023");
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
   });
 
-  test("should have patient Françoise Fournier", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
-    const line = page.locator("tr", { hasText: "Françoise Fournier" });
-    const cells = line.locator("td");
-    await expect(cells.nth(0)).toHaveText("Françoise Fournier");
-    await expect(cells.nth(1)).toHaveText("Post PVC Ablation");
-    await expect(cells.nth(2)).toHaveText("31");
-    await expect(cells.nth(3)).toHaveText("3/18/2023");
-  });
+  const testCases = [
+    { name: "Laurence Fournier", indication: "Post TAVI", age: "21", date: "3/12/2023" },
+    { name: "Françoise Fournier", indication: "Post PVC Ablation", age: "31", date: "3/18/2023" },
+    { name: "Clotilde Sanchez", indication: "Palpitations", age: "38", date: "1/25/2023" },
+  ];
 
-  test("should have patient Clotilde Sanchez", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
-    const line = page.locator("tr", { hasText: "Clotilde Sanchez" });
-    const cells = line.locator("td");
-    await expect(cells.nth(0)).toHaveText("Clotilde Sanchez");
-    await expect(cells.nth(1)).toHaveText("Palpitations");
-    await expect(cells.nth(2)).toHaveText("38");
-    await expect(cells.nth(3)).toHaveText("1/25/2023");
-  });
+  for (const testCase of testCases) {
+    test(`should have patient ${testCase.name}`, async ({ page }) => {
+      const line = page.locator(`tr:has-text("${testCase.name}")`);
+      const cells = line.locator("td");
+
+      await expect(cells.nth(0)).toHaveText(testCase.name);
+      await expect(cells.nth(1)).toHaveText(testCase.indication);
+      await expect(cells.nth(2)).toHaveText(testCase.age);
+      await expect(cells.nth(3)).toHaveText(testCase.date);
+    });
+  }
 });
 
 test.describe("Filters", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto(BASE_URL);
+  });
+
   test("should filter by patient name", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
-    await page.waitForTimeout(2000);
-    await page.mouse.click(120, 175);
-    await page.keyboard.type("rou");
+    await page.locator('xpath=//input[@class="chakra-input css-179hgeo"]').fill("rou")
 
     const lines = page.locator("tr");
     await expect(lines).toHaveCount(4);
@@ -53,23 +48,11 @@ test.describe("Filters", () => {
   });
 
   test("should filter by indication", async ({ page }) => {
-    await page.goto("http://localhost:5173/");
-    await page.waitForTimeout(2000);
-    await page.mouse.click(120, 175);
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("P");
-    await page.keyboard.press("P");
-
+    await page.locator('select[class="chakra-select css-9zgf1d"]').selectOption('palpitations');
+    
     const lines = page.locator("tr");
-    await expect(lines.nth(1).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(2).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(3).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(4).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(5).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(6).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(7).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(8).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(9).locator("td").nth(1)).toHaveText("Palpitations");
-    await expect(lines.nth(10).locator("td").nth(1)).toHaveText("Palpitations");
+    for (let i = 1; i <= 10; i++) {
+      await expect(lines.nth(i).locator("td").nth(1)).toHaveText("Palpitations");
+    }
   });
 });
